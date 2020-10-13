884a79adfbdeba9d6fe9027cda1bc7a33396e4d4
范轶洁 <fanyjb@digitalchina.com>
2020-10-13 19:29:20 +0800

    第一个redux程序

---
+++ docs/redux.md
@@ -0,0 +1,15 @@
+# Redux 培训
+
+## 核心概念
+
+- 单一不可变状态树
+- 唯一改变状态树的方式是 分发一个 action
+- 纯函数
+  - 同样参数 同样输出
+  - 不修改参数
+- UI
+  - 描述成 纯函数
+  - 参数 是 app state
+- Reducer
+  - last state
+  - next state
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,9 +1,58 @@
 <template>
-  <h1>Todos</h1>
+  <main>
+    <h1>{{ count }}</h1>
+    <button @click="INCREMENT">INCREMENT</button>
+    <button @click="DECREMENT">DECREMENT</button>
+  </main>
 </template>
 
 <script>
-export default {};
+import { createStore } from 'redux';
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  counter,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
+
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    INCREMENT() {
+      store.dispatch({ type: 'INCREMENT' });
+    },
+    DECREMENT() {
+      store.dispatch({ type: 'DECREMENT' });
+    },
+  },
+  mounted() {
+    console.log(store.getState());
+
+    store.subscribe(() => {
+      this.count = store.getState();
+    });
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+* {
+  border: 1px solid black;
+}
+</style>
