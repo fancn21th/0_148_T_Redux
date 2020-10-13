2ead9f7d8edec9663f3f1636d26f3d53bb465fd7
范轶洁 <fanyjb@digitalchina.com>
2020-10-13 11:48:53 +0800

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Todos msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Todos from "./components/Todos.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
-  },
+    Todos
+  }
 };
 </script>
 
