685f8af3a6ca5f5789e0c6193fd964dd5c12d11f
范轶洁 <fanyjb@digitalchina.com>
2020-10-14 11:23:42 +0800

    简单的 自定义实现

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
+    // '@vue/cli-plugin-babel/preset',
+    ['@vue/app', { useBuiltIns: 'entry' }], // https://github.com/vuejs/vue-cli/issues/3678
   ],
 };
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -7,7 +7,36 @@
 </template>
 
 <script>
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+
+// 自定义实现
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  // 初始化 state
+  dispatch({});
+
+  return {
+    getState,
+    dispatch,
+    subscribe,
+  };
+};
 
 const counter = (state = 0, action) => {
   switch (action.type) {
@@ -23,7 +52,7 @@ const counter = (state = 0, action) => {
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
   counter,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 /* eslint-enable */
 
